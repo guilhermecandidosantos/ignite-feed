86fde2d48a246e40ef7eef0edb50972969df3277
Guilherme Cândido dos Santos <guilhermecandido1998@gmail.com>
2023-08-27 12:19:53 -0300

    Create Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post />
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>
+          <Post />
+        </main>
+      </div>
     </div>
   )
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,3 +1,8 @@
 export function Post() {
-  return <p>Post</p>
+  return (
+    <div>
+      <h1>Guilherme Santos</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut felis bibendum, accumsan libero quis, pulvinar ex. Mauris dapibus congue gravida. Vivamus porta, augue vitae venenatis sollicitudin, lorem elit gravida dolor, non rutrum sapien erat quis mi. Nullam accumsan sodales ante vitae euismod. Morbi vel velit tempus, finibus lectus eget, tincidunt augue. Morbi imperdiet placerat ligula, nec volutpat ex. Nam in mollis est, in maximus eros. Maecenas efficitur molestie ornare.</p>
+    </div>
+  )
 }
